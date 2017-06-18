b64b45bf86edd21ba475676e136e34e99d0fdb24
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2017-06-17 23:21:55 -0400

    Add files via upload

--- field_calculator/date_time_defs.py
+++ field_calculator/date_time_defs.py
@@ -9,6 +9,29 @@
 """
 from datetime import datetime
 
+# -*- coding: utf-8 -*-
+"""
+Script:  DDddd_DMS_convert.py
+Author:  Dan.Patterson@carleton.ca
+
+Purpose: Formatting stuff
+Notes:
+- to use in the field calculator, set the parser to Python and
+  use ' !fieldname! ' in the expression box
+- degree sign, ° ... ALT 248 on the numeric keypad
+- utf-8  u'\N{DEGREE SIGN}' or u'\xb0'
+"""
+
+def ddd_dms(a):
+    """Decimal degree to DMS format"""
+    sign = [-1, 1][a > 0]
+    DD, dd = divmod(a, sign)
+    MM, ss = divmod(dd*60, sign)
+    SS, ssss = divmod(ss*60, 1)
+    frmt = "{:0= 4}" + u'\xb0' + " {:=2}' {:0=7.4f}\" "
+    DMS = frmt.format(int(sign*DD), int(MM), sign*ss*60)
+    return DMS
+
 
 def get_date(fld):
     """input a date field, strip off the time and format
@@ -34,10 +57,27 @@ def get_time(fld):
     else:
         return None
 
-# ----------------------------------------------------------------------
+def _demo():
+    """
+    :other format options
+    :mess with the order of line 44 for different outputs
+    """
+    today = datetime.today()
+    print('\n_demo def...\nISO     :', today)
+    print('format(): {:%a %b %d %H:%M:%S %Y}'.format(today))
+    #return today
+
+#--------------------------------------------------------------------
 # __main__ .... code section
 if __name__ == "__main__":
     """ A simple test """
     n = str(datetime.now())
-    print("\nDate from {} ... {}".format(n, get_date(n)))
-    print("\nDate from {} ... {}".format(n, get_time(n)))
+    print("\nget_date def...\nDate from {} ... {}".format(n, get_date(n)))
+    print("\nget_time def...\nDate from {} ... {}".format(n, get_time(n)))
+    _demo()
+
+    vals = [45.501234567890, -45.501234567890,
+            145.501234567890, -145.501234567890]
+    print("\nddd_dms def...\nTest run with a in vals")
+    for a in vals:
+        print("{:> 22.12f} ... {!s:>20}".format(a, ddd_dms(a)))
